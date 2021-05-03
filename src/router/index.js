ea610f7c545b7dfbdd7468810689f58079ee4a57
rykesz <adfourretout@hotmail.fr>
2021-05-03 10:49:00 +0200

    maj router

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '@/views/Home.vue'
+import Fil from '@/views/Fil.vue'
+import Profil from '@/views/Profil.vue'
+import CreatePublication from '@/views/CreatePublication.vue'
 
 const routes = [
   {
