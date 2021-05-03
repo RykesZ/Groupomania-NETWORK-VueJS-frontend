bf7877b76fd3b820b322eef428fb0b08b730f499
rykesz <adfourretout@hotmail.fr>
2021-05-03 10:30:07 +0200

    router config

--- src/router/index.js
+++ src/router/index.js
@@ -13,7 +13,17 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Fil.vue')
+    component: () => import('@/views/Fil.vue')
+  },
+  {
+    path: '/profil',
+    name: 'Profil',
+    component: () => import('@/views/Profil.vue')
+  },
+  {
+    path: '/create',
+    name: 'CreatePublication',
+    component: () => import('@/views/CreatePublication.vue')
   }
 ]
 
