1d881b01f4bf7218d8269bd9bad9a884f04b786d
rykesz <adfourretout@hotmail.fr>
2021-06-08 22:42:23 +0200

    pagination finie

--- src/components/Frames/Pagination.vue
+++ src/components/Frames/Pagination.vue
@@ -2,14 +2,18 @@
     <div class="pagination">
         <!-- |< -->
         <button ref="firstPagePaginationButton" class="paginationButton" @click="$emit('emit-first-page-value', 1), changeActiveButton($event)"><span class="material-icons">first_page</span></button>
+        <!-- < -->
+        <button v-if="activePreviousButton" ref="previousPagePaginationButton" class="paginationButton" @click="$emit('emit-previous-page-value', previousPageValue), changeActiveButton($event)"><span class="material-icons">navigate_before</span></button>
         <!-- 1 -->
-        <button ref="firstPaginationButton" class="paginationButton" @click="$emit('emit-first-button-value', firstButtonValue), changeActiveButton($event)"><span>{{ firstButtonValue }}</span></button>
+        <button ref="firstPaginationButton" class="paginationButton active" @click="$emit('emit-first-button-value', firstButtonValue), changeActiveButton($event)"><span>{{ firstButtonValue }}</span></button>
         <!-- 2 -->
         <button v-if="activeSecondButton" ref="secondPaginationButton" class="paginationButton" @click="$emit('emit-second-button-value', secondButtonValue), changeActiveButton($event)"><span>{{ secondButtonValue }}</span></button>
         <!-- 3 -->
         <button v-if="activeThirdButton" ref="thirdPaginationButton" class="paginationButton" @click="$emit('emit-third-button-value', thirdButtonValue), changeActiveButton($event)"><span>{{ thirdButtonValue }}</span></button>
         <!-- ... -->
-        <button class="paginationButton"><span>...</span></button>
+        <button v-if="activeSearchButton" class="paginationButton" @click="searchPage"><span>...</span></button>
+        <!-- > -->
+        <button v-if="activeNextButton" ref="nextPagePaginationButton" class="paginationButton" @click="$emit('emit-next-page-value', nextPageValue), changeActiveButton($event)"><span class="material-icons">navigate_next</span></button>
         <!-- >| -->
         <button ref="lastPagePaginationButton" class="paginationButton" @click="$emit('emit-last-page-value', 'max'), changeActiveButton($event)"><span class="material-icons">last_page</span></button>
     </div>
@@ -18,11 +22,15 @@
 <script>
 export default {
     name: 'Pagination',
-    emits: ['emit-first-page-value', 'emit-first-button-value', 'emit-second-button-value', 'emit-third-button-value', 'emit-last-page-value'],
+    emits: ['emit-first-page-value', 'emit-first-button-value', 'emit-second-button-value', 'emit-third-button-value', 'emit-last-page-value','emit-previous-page-value', 'emit-next-page-value', 'emit-search-page-value'],
     data() {
         return {
+            activePreviousButton: false,
             activeSecondButton: false,
             activeThirdButton: false,
+            activeSearchButton: false,
+            activeNextButton: false,
+            activeButton: 1
         }
     },
     props: {
@@ -33,16 +41,97 @@ export default {
     },
     methods: {
         changeActiveButton(event) {
-            console.log('coucou');
-            this.$refs.firstPagePaginationButton.classList.remove('active');
-            this.$refs.firstPaginationButton.classList.remove('active');
-            this.$refs.secondPaginationButton.classList.remove('active');
-            this.$refs.thirdPaginationButton.classList.remove('active');
-            this.$refs.lastPagePaginationButton.classList.remove('active');
-            event.target.classList.add('active');
+            if ((event.target == this.$refs.firstPaginationButton && this.firstButtonValue == 1) || event.target == this.$refs.firstPagePaginationButton || (event.target == this.$refs.previousPagePaginationButton && this.secondButtonValue == 2)) {
+                // Désactive tous les boutons FACTORISER TOUT CA EN UNE SEULE FONCTION QUI ACTIVE/DESACTIVE
+                this.$refs.firstPagePaginationButton.classList.remove('active');
+                this.$refs.firstPaginationButton.classList.remove('active');
+                this.$refs.secondPaginationButton.classList.remove('active');
+                this.$refs.thirdPaginationButton.classList.remove('active');
+                this.$refs.lastPagePaginationButton.classList.remove('active');
+                // Active le 1er bouton
+                this.$refs.firstPaginationButton.classList.add('active');
+                this.activeButton = 1;
+            } else if (event.target == this.$refs.lastPagePaginationButton || (event.target == this.$refs.nextPagePaginationButton && this.thirdButtonValue == Math.ceil(this.allPubliLength / 10))) {
+                // Désactive tous les boutons
+                this.$refs.firstPagePaginationButton.classList.remove('active');
+                this.$refs.firstPaginationButton.classList.remove('active');
+                this.$refs.secondPaginationButton.classList.remove('active');
+                this.$refs.thirdPaginationButton.classList.remove('active');
+                this.$refs.lastPagePaginationButton.classList.remove('active');
+                // Active le 3eme bouton
+                this.$refs.thirdPaginationButton.classList.add('active');
+                this.activeButton = 3;
+            } else {
+                // Désactive tous les boutons
+                this.$refs.firstPagePaginationButton.classList.remove('active');
+                this.$refs.firstPaginationButton.classList.remove('active');
+                this.$refs.secondPaginationButton.classList.remove('active');
+                this.$refs.thirdPaginationButton.classList.remove('active');
+                this.$refs.lastPagePaginationButton.classList.remove('active');
+                // Active le 2eme bouton
+                this.$refs.secondPaginationButton.classList.add('active');
+                this.activeButton = 2;
+            }
+        },
+        searchPage() {
+            let pageNumber = prompt("Entrez le numéro de la page à laquelle vous souhaitez accéder :");
+            if ((pageNumber != null || pageNumber != "") && pageNumber.match(/^[0-9]+$/) ) {
+                this.$emit('emit-search-page-value', pageNumber);
+
+                if (pageNumber == Math.ceil(this.allPubliLength / 10) || pageNumber > Math.ceil(this.allPubliLength / 10)) {
+                    // Désactive tous les boutons
+                    this.$refs.firstPagePaginationButton.classList.remove('active');
+                    this.$refs.firstPaginationButton.classList.remove('active');
+                    this.$refs.secondPaginationButton.classList.remove('active');
+                    this.$refs.thirdPaginationButton.classList.remove('active');
+                    this.$refs.lastPagePaginationButton.classList.remove('active');
+                    // Active le 3eme bouton
+                    this.$refs.thirdPaginationButton.classList.add('active');
+                    this.activeButton = 3;
+                } else if (pageNumber < Math.ceil(this.allPubliLength / 10) && pageNumber > 1) {
+                    // Désactive tous les boutons
+                    this.$refs.firstPagePaginationButton.classList.remove('active');
+                    this.$refs.firstPaginationButton.classList.remove('active');
+                    this.$refs.secondPaginationButton.classList.remove('active');
+                    this.$refs.thirdPaginationButton.classList.remove('active');
+                    this.$refs.lastPagePaginationButton.classList.remove('active');
+                    // Active le 2eme bouton
+                    this.$refs.secondPaginationButton.classList.add('active');
+                    this.activeButton = 2;
+                } else if (pageNumber == 1) {
+                    // Désactive tous les boutons FACTORISER TOUT CA EN UNE SEULE FONCTION QUI ACTIVE/DESACTIVE
+                    this.$refs.firstPagePaginationButton.classList.remove('active');
+                    this.$refs.firstPaginationButton.classList.remove('active');
+                    this.$refs.secondPaginationButton.classList.remove('active');
+                    this.$refs.thirdPaginationButton.classList.remove('active');
+                    this.$refs.lastPagePaginationButton.classList.remove('active');
+                    // Active le 1er bouton
+                    this.$refs.firstPaginationButton.classList.add('active');
+                    this.activeButton = 1;
+                }
+
+
+            } else {
+                window.alert("La valeur entrée est incorrecte")
+            }
         }
     },
     computed: {
+        previousPageValue() {
+            if (this.activeButton == 3) {
+                return this.secondButtonValue;
+            } else {
+                return this.secondButtonValue - 1;
+            }
+        },
+        nextPageValue() {
+            if (this.activeButton == 1) {
+                return this.secondButtonValue;
+            } else {
+                return this.secondButtonValue + 1;
+            }
+            
+        }
     },
     watch: {
         allPubliLength() {
@@ -50,6 +139,11 @@ export default {
                 this.activeSecondButton = true;
                 if (this.allPubliLength > 20) {
                     this.activeThirdButton = true;
+                    if (this.allPubliLength > 30) {
+                        this.activeSearchButton = true;
+                    } else {
+                        this.activeSearchButton = false;
+                    }
                 } else {
                     this.activeThirdButton = false;
                 }
@@ -57,6 +151,33 @@ export default {
                 this.activeSecondButton = false;
                 this.activeThirdButton = false;
             }
+        },
+        firstButtonValue() {
+            if (this.firstButtonValue > 1 || this.activeButton > 1) {
+                this.activePreviousButton = true;
+            } else {
+                this.activePreviousButton = false;
+            }
+        },
+        activeButton() {
+            if (this.firstButtonValue > 1 || this.activeButton > 1) {
+                this.activePreviousButton = true;
+            } else {
+                this.activePreviousButton = false;
+            }
+
+            if (this.activeButton == 3) {
+                this.activeNextButton = false;
+            } else {
+                this.activeNextButton = true;
+            }
+        }
+    },
+    beforeMount() {
+        if (this.secondButtonValue > 1) {
+            this.activeNextButton = true;
+        } else {
+            this.activeNextButton = false;
         }
     }
 }
--- src/views/Fil.vue
+++ src/views/Fil.vue
@@ -1,7 +1,7 @@
 <template>
     <Header :active="active" @emit-reload-main-thread="reloadMainThread"/>
     <FilContent ref="filContent" @emit-go-to-create-publi="goToCreatePubli" @emit-redirect-modify-publi="redirectModifyPubli" @emit-new-all-publi-length="setNewAllPubliLength" :key="reloadFilContentKey" />
-    <Pagination @emit-first-page-value="goToPage" @emit-first-button-value="goToPage" @emit-second-button-value="goToPage" @emit-last-page-value="goToPage" :firstButtonValue="firstButtonValue" :secondButtonValue="secondButtonValue" :thirdButtonValue="thirdButtonValue" :allPubliLength="allPubliLength"/>
+    <Pagination @emit-first-page-value="goToPage" @emit-previous-page-value="goToPage" @emit-first-button-value="goToPage" @emit-second-button-value="goToPage" @emit-third-button-value="goToPage" @emit-search-page-value="goToPage" @emit-next-page-value="goToPage" @emit-last-page-value="goToPage" :firstButtonValue="firstButtonValue" :secondButtonValue="secondButtonValue" :thirdButtonValue="thirdButtonValue" :allPubliLength="allPubliLength"/>
 </template>
 
 <script>
@@ -51,11 +51,26 @@ export default {
         goToPage(pageNumber) {
             console.log('click');
             if (pageNumber) {
+                if (pageNumber > Math.ceil(this.allPubliLength / 10)) {
+                    pageNumber = Math.ceil(this.allPubliLength / 10);
+                }
                 this.$refs.filContent.updateFilContent(pageNumber);
-                if (pageNumber > 1) {
+                if (pageNumber == 1) {
+                    this.firstButtonValue = pageNumber;
+                    this.secondButtonValue = pageNumber + 1;
+                    this.thirdButtonValue = pageNumber + 2;
+                } else if (pageNumber > 1 && pageNumber < Math.ceil(this.allPubliLength / 10)) {
                     this.firstButtonValue = pageNumber - 1;
                     this.secondButtonValue = pageNumber;
                     this.thirdButtonValue = pageNumber + 1;
+                } else if (pageNumber == 'max' && this.allPubliLength > 20) {
+                        this.thirdButtonValue = Math.ceil(this.allPubliLength / 10);
+                        this.secondButtonValue = this.thirdButtonValue - 1;
+                        this.firstButtonValue = this.secondButtonValue - 1;
+                } else if (pageNumber > 1 && pageNumber == Math.ceil(this.allPubliLength / 10)) {
+                    this.firstButtonValue = pageNumber - 2;
+                    this.secondButtonValue = pageNumber - 1;
+                    this.thirdButtonValue = pageNumber;
                 }
             } else {
                 return 1;
